8ad605f0a105297128ff3d840f9e4099088ade26
Anna <Gorenskayaanna@mail.com>
2022-12-16 22:26:11 +0300

    Square B

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+double a, b, square;
+Console.WriteLine("Enter first number: ");
+a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter second number: ");
+b = Convert.ToDouble(Console.ReadLine());
+square = b * b;
+if (a == square)
+{
+    Console.WriteLine("A equals square B");
+    Console.WriteLine("Square B is: "+square);
+}
+else
+{
+    Console.WriteLine("A isn't equals square B");
+    Console.WriteLine("Square B is: "+square);
+}
